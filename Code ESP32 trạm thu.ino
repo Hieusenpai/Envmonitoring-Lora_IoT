8e0efdafcbe8b98941508a535460f3747e6335ae
Hieusenpai <106915167+Hieusenpai@users.noreply.github.com>
2025-10-21 20:26:32 +0700

    Update Code ESP32 trạm thu.ino

--- Code ESP32 trạm thu.ino
+++ Code ESP32 trạm thu.ino
@@ -1,18 +1,55 @@
+/*
+   LoRa E32-TTL-100
+   Receive fixed transmission message as a specified point.
+   https://www.mischianti.org/2019/11/10/lora-e32-device-for-arduino-esp32-or-esp8266-fixed-transmission-part-4/
+  
+  Update:
+          + 04/04/2024: Sửa lại code nhằm phù hợp với BLynk phiên bản mới
+  Địa chỉ:
+          - configuration.ADDL = 6;
+          - configuration.ADDH = 0;
+          - configuration.CHAN = 0x09;
+
+   LIBRARY
+   - TFT_eSPI (2.5.43)
+
+*/
+
 TaskHandle_t Task2;
 
 #include "iconSuDung.h"
-#include <TFT_eSPI.h> // Graphics and font library for ST7735 driver chip
+//#include <TFT_eSPI.h> // Graphics and font library for ST7735 driver chip
 #include <SPI.h>
-TFT_eSPI tft = TFT_eSPI();  // Invoke library, pins defined in User_Setup.h
+//TFT_eSPI tft = TFT_eSPI();  // Invoke library, pins defined in User_Setup.h
 
-#include "LoRa_E32.h"
-LoRa_E32 e32ttl(&Serial2); //  RX AUX M0 M1 (ESP32)
+#include <Adafruit_GFX.h>     // Core graphics library
+#include <Adafruit_ST7735.h>  // Hardware-specific library for ST7735
+#include <Adafruit_ST7789.h>  // Hardware-specific library for ST7789
+#include <SPI.h>
+
+#if defined(ARDUINO_FEATHER_ESP32)  // Feather Huzzah32
+#define TFT_CS 15
+#define TFT_RST 4
+#define TFT_DC 2
+
+#elif defined(ESP8266)
+#define TFT_CS 4
+#define TFT_RST 16
+#define TFT_DC 5
+
+#else
+// For the breakout board, you can use any 2 or 3 pins.
+// These pins will also work for the 1.8" TFT shield.
+#define TFT_CS 15
+#define TFT_RST 4  // Or set to -1 and connect to Arduino RESET pin
+#define TFT_DC 2
+#endif
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 
 //===== Blynk IOT =====//
-#define BLYNK_TEMPLATE_ID           "TMPxxxxxx"
-#define BLYNK_TEMPLATE_NAME         "Device"
-#define BLYNK_AUTH_TOKEN            "YourAuthToken"
-
+#define BLYNK_TEMPLATE_ID "TMPL6px07Al-A"
+#define BLYNK_TEMPLATE_NAME "do an"
+#define BLYNK_AUTH_TOKEN "Tmx16VjEhxsQhgO0s2Q59lQgbqtl9TVb"
 /* Comment this out to disable prints and save space */
 #define BLYNK_PRINT Serial
 
@@ -26,14 +63,13 @@ char auth[] = BLYNK_AUTH_TOKEN;
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
-char ssid[] = "nhom 1";
-char pass[] = "12345679";
+char ssid[] = "FPT Telecom-E01E";
+char pass[] = "12341234";
 TaskHandle_t Core2;
 BlynkTimer timer;
 WidgetRTC rtc;
 
-void clockDisplay()
-{
+void clockDisplay() {
   String currentTime = String(hour()) + ":" + minute() + ":" + second();
   String currentDate = String(day()) + " " + month() + " " + year();
 }
@@ -43,9 +79,9 @@ BLYNK_CONNECTED() {
 }
 
 
-#define ledRun   21
-#define ledAlarm 5
-#define buzz     12
+#define ledRun 5
+#define ledAlarm 21
+#define buzz 12
 
 
 // -------------------------------------
@@ -58,36 +94,38 @@ long t1, t2, t3, t4, t5;
 String str = "";
 
 float
-nhietdo1,
-doam1   ,
-bui1    ,
-dienap1 ,
-nhietdo2,
-doam2   ,
-bui2    ,
-dienap2 ,
-buiHigh = 120.00,
-batLow  = 3.60;
+  nhietdo1,
+  doam1,
+  bui1,
+  dienap1,
+  nhietdo2,
+  doam2,
+  bui2,
+  dienap2,
+  buiHigh = 120.00,
+  batLow = 3.60;
 
 unsigned long
-previousMillis        = 0,
-currentMillis         = 0,
-prevSerialMillis      = 0,
-currentSerialMillis   = 0,
-prevBlynkMillis      = 0,
-currentBlynkMillis = 0;
+  previousMillis = 0,
+  currentMillis = 0,
+  prevSerialMillis = 0,
+  currentSerialMillis = 0,
+  prevBlynkMillis = 0,
+  currentBlynkMillis = 0;
 
 int
-ledState   = 0,
-mauCanhBao = 0,
-ERR        = 0,
-ERR2       = 0;
+  ledState = 0,
+  mauCanhBao = 0,
+  ERR = 0,
+  ERR2 = 0;
 
+#define UART2_TX 17  // TX2
+#define UART2_RX 16  // RX2
+#define BAUDRATE 9600
 
-void setup()
-{
+void setup() {
   Serial.begin(115200);
-
+  Serial2.begin(BAUDRATE, SERIAL_8N1, UART2_RX, UART2_TX);
   pinMode(ledRun, OUTPUT);
   pinMode(ledAlarm, OUTPUT);
   pinMode(buzz, OUTPUT);
@@ -95,39 +133,47 @@ void setup()
   digitalWrite(ledRun, HIGH);
   digitalWrite(ledAlarm, HIGH);
 
-  tft.init();
+  tft.initR(INITR_BLACKTAB);
   tft.setRotation(3);
-  tft.fillScreen(TFT_BLACK);
-  e32ttl.begin();
+  tft.fillScreen(ST77XX_BLACK);
+
 
   // ---------------------------
 
 
   xTaskCreatePinnedToCore(
-    Task2code,   /* Task function. */
-    "Task2",     /* name of task. */
-    10000,       /* Stack size of task */
-    NULL,        /* parameter of the task */
-    1,           /* priority of the task */
-    &Task2,      /* Task handle to keep track of created task */
-    1);          /* pin task to core 1 */
+    Task2code, /* Task function. */
+    "Task2",   /* name of task. */
+    10000,     /* Stack size of task */
+    NULL,      /* parameter of the task */
+    1,         /* priority of the task */
+    &Task2,    /* Task handle to keep track of created task */
+    1);        /* pin task to core 1 */
 
-} // end Setup
-
-void Task2code( void * pvParameters ) {
+}  // end Setup
+uint32_t send_data = 0;
+void Task2code(void* pvParameters) {
   Blynk.begin(auth, ssid, pass);
-  setSyncInterval(5); // Sync interval in seconds (10 minutes)
+  setSyncInterval(5);  // Sync interval in seconds (10 minutes)
   while (1) {
-    senBlynk();
+    if (millis() - send_data > 1000) {
+      send_data = millis();
+      senBlynk();
+       Serial.print("send Blynk ! \r\n");
+    }
     Blynk.run();
     timer.run();
+    vTaskDelay(100);
   }
 }
-
-void loop()
-{
+uint32_t timer_sceen = 0;
+void loop() {
   NhanDuLieu();
-  TFT_Print();
+  if (millis() - timer_sceen > 500) {
+    timer_sceen = millis();
+    TFT_Print();
+  }
+
   Onboard_Telemetry();
   CanhBao();
-} // end loop
+}  // end loop
