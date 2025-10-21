433c557891880b161c4f92db34d89a8c90faf9b5
Hieusenpai <106915167+Hieusenpai@users.noreply.github.com>
2025-10-21 20:30:09 +0700

    Update Code trạm phát.ino

--- Code trạm phát.ino
+++ Code trạm phát.ino
@@ -1,26 +1,32 @@
 #include "LoRa_E32.h"
-LoRa_E32 e32ttl(3, 2);
+#include <SoftwareSerial.h>
+
+const byte rxPin = 3;
+const byte txPin = 2;
+// Set up a new SoftwareSerial object
+SoftwareSerial mySerial(rxPin, txPin);
+
 
 #include "DHT.h"
-#define DHTPIN 6     // what digital pin we're connected to
-#define DHTTYPE DHT11   // DHT 11
+#define DHTPIN 6       // what digital pin we're connected to
+#define DHTTYPE DHT11  // DHT 11
 DHT dht(DHTPIN, DHTTYPE);
 
-#define tbi 1       //  Bộ phát số 1
+#define tbi 1  //  Bộ phát số 1
 
 int
-volPin = A1,
-tt,
-hh,
-dienap;
+  volPin = A1,
+  tt,
+  hh,
+  dienap;
 
 float
-h,
-t,
-vout = 0,
-vin  = 0,
-R1 = 30000.0,
-R2 = 7200.0;
+  h,
+  t,
+  vout = 0,
+  vin = 0,
+  R1 = 30000.0,
+  R2 = 7200.0;
 
 int dustPin = A0;
 int ledPower = 10;
@@ -28,79 +34,79 @@ int ledPower = 10;
 int dustVal = 0;
 char s[32];
 float
-dustdensity = 0.0000,
-dustRaw     = 0.0000,
-voltage     = 0.0000;
+  dustdensity = 0.0000,
+  dustRaw = 0.0000,
+  voltage = 0.0000;
 
 int avgDust = 10;
 
-void setup()
-{
+void setup() {
   Serial.begin(9600);
   pinMode(ledPower, OUTPUT);
   dht.begin();
-  e32ttl.begin();
+  //e32ttl.begin();.
+  mySerial.begin(9600);
+ 
 }
 
 // The loop function is called in an endless loop
-void loop()
-{
+void loop() {
   h = dht.readHumidity();
   t = dht.readTemperature();
 
-  vin = analogRead(volPin) * (5.0 / 1023.0); // see text
-  hh  = h * 10;
-  tt  = t * 10;
-  dienap  = vin * 0.992 * 100;
+  vin = analogRead(volPin) * (5.0 / 1023.0);  // see text
+  hh = h * 10;
+  tt = t * 10;
+  dienap = vin * 0.992 * 100;
 
   cambienbui();
-  int bui =  dustdensity * 10.0;
+  int bui = dustdensity * 10.0;
 
-  Serial.print("Temp : ");  Serial.print(t);
-  Serial.print(" Hum  : "); Serial.print(h);
-  Serial.print(" PM2.5: "); Serial.print(dustdensity);
-  Serial.print(" Vol  : "); Serial.println(vin * 0.992);
+  Serial.print("Temp : ");
+  Serial.print(t);
+  Serial.print(" Hum  : ");
+  Serial.print(h);
+  Serial.print(" PM2.5: ");
+  Serial.print(dustdensity);
+  Serial.print(" Vol  : ");
+  Serial.println(vin * 0.992);
 
   if (runEvery(5000)) {
     String mess1 = 'a' + String(tbi) + 'b' + String(tt) + 'c' + String(hh) + 'd' + String(bui) + 'e' + String(dienap) + 'f';
-    //Serial.println(mess1);
-    ResponseStatus rs = e32ttl.sendFixedMessage(0, 6, 0x09, mess1);
+    Serial.println(mess1);
+    mySerial.print(mess1);
   }
-  
 }
 
-boolean runEvery(unsigned long interval)
-{
+boolean runEvery(unsigned long interval) {
   static unsigned long previousMillis = 0;
   unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= interval)
-  {
+  if (currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
     return true;
   }
   return false;
 }
 
-void cambienbui()
-{
+void cambienbui() {
   // if (runEvery(1000)) {
-  digitalWrite(ledPower, LOW); // power on the LED
+  digitalWrite(ledPower, LOW);  // power on the LED
   delayMicroseconds(280);
-  dustVal = analogRead(dustPin); // read the dust value
+  dustVal = analogRead(dustPin);  // read the dust value
   delayMicroseconds(40);
-  digitalWrite(ledPower, HIGH); // turn the LED off
+  digitalWrite(ledPower, HIGH);  // turn the LED off
   delayMicroseconds(9680);
 
-  voltage = (dustVal * 5 / 1024.0); //dustval*5/1024
+  voltage = (dustVal * 5 / 1024.0);  //dustval*5/1024
 
   dustRaw = 0.0000;
   for (int i = 0; i < avgDust; i++) {
     //dustRaw = dustRaw + 57*voltage;
     dustRaw = dustRaw + 0.17 * voltage - 0.1;
   }
-  dustdensity = (dustRaw / avgDust) * 1000.0; // ug/m3
+  dustdensity = (dustRaw / avgDust) * 1000.0;  // ug/m3
 
-  if (dustdensity < 0 )
+  if (dustdensity < 0)
     dustdensity = 0;
   if (dustdensity > 500)
     dustdensity = 500;
