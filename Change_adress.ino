de94f96141a3f55b6fbd92ea9f277861ac0fbcd0
Hieusenpai <106915167+Hieusenpai@users.noreply.github.com>
2025-11-03 13:42:42 +0700

    Update Change_adress.ino

--- Change_adress.ino
+++ Change_adress.ino
@@ -1,5 +1,6 @@
 // ESP32 Serial Bridge (UART0 <-> UART2)
 // tải file RF_Setting_EN_V2.7 để công tắc 2p mức cao để setup 2 modul lora rồi mới upload code.
+// https://drive.google.com/file/d/1q_46MbhaRAg3XRTuCr7Jh-fm0n57mq8G/view?usp=sharing
 #define UART2_TX 17  // TX2
 #define UART2_RX 16  // RX2
 #define BAUDRATE 9600
