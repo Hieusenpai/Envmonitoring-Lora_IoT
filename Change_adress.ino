0e1de58a25ba2a43a35a723c8e59e1af6502a8d2
Hieusenpai <106915167+Hieusenpai@users.noreply.github.com>
2025-11-03 13:35:30 +0700

    Create Change_adress.ino

---
+++ Change_adress.ino
@@ -0,0 +1,29 @@
+// ESP32 Serial Bridge (UART0 <-> UART2)
+
+#define UART2_TX 17  // TX2
+#define UART2_RX 16  // RX2
+#define BAUDRATE 9600
+
+void setup() {
+  // Khởi tạo UART0 (Serial USB)
+  Serial.begin(BAUDRATE);
+
+  // Khởi tạo UART2
+  Serial2.begin(BAUDRATE, SERIAL_8N1, UART2_RX, UART2_TX);
+
+ // Serial.println("UART Bridge ESP32 (UART0 <-> UART2) started");
+}
+
+void loop() {
+  // Nhận từ UART0 và gửi sang UART2
+  while (Serial.available()) {
+    int c = Serial.read();
+    Serial2.write(c);
+  }
+
+  // Nhận từ UART2 và gửi sang UART0
+  while (Serial2.available()) {
+    int c = Serial2.read();
+    Serial.write(c);
+  }
+}
